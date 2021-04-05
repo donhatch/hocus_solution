ce3b8fc5fb7bd94610e83c32e8f9ece27a3e213a
Don Hatch <hatch@plunk.org>
2021-04-05 00:09:14 -0700

    wip

--- hocus.py
+++ hocus.py
@@ -374,10 +374,40 @@ namesAndInputs = [
     '   \|  ',
     '    *  ',
   ]],
-  #['input21', [
-  #]],
-  #['input22', [
-  #]],
+  ['input21', [
+    '  *    ',
+    '  |\   ',
+    '* | \  ',
+    ' \|  \ ',
+    '  |   *',
+    '  |\  |',
+    '* | * |',
+    ' \| | |',
+    '  \ | |',
+    '  |\| |',
+    '  | | |',
+    '  | |\|',
+    '  * | *',
+    '   \|  ',
+    '    *  ',
+  ]],
+  ['input22', [
+    '      *',
+    '     /|',
+    '    * |',
+    '    | |',
+    '    | *',
+    '    |/|',
+    '    | |',
+    '   /| |',
+    '  * | |',
+    ' / \| |',
+    '*   \ |',
+    ' \  |\|',
+    '  \ | *',
+    '   \|  ',
+    '    *  ',
+  ]],
   ['input23', [
     '    *  ',
     '   /|\ ',
@@ -395,6 +425,139 @@ namesAndInputs = [
     ' \|/   ',
     '  *    ',
   ]],
+  ['input24', [
+    '    *    ',
+    '   / \   ',
+    '  /   \  ',
+    ' /     \ ',
+    '*   *   *',
+    '|\ / \ /|',
+    '| \   \ |',
+    '|/ \ / \|',
+    '*   *   *',
+    ' \     / ',
+    '  \   /  ',
+    '   \ /   ',
+    '    *    ',
+  ]],
+  ['input25', [
+    '    *    ',
+    '   /|\   ',
+    '  * | *  ',
+    ' /| | |\ ',
+    '* | | | *',
+    ' \| | |/ ',
+    '  * | *  ',
+    '   \|/   ',
+    '    *    ',
+  ]],
+  ['input26', [
+    '*    ',
+    '|\   ',
+    '| *  ',
+    '| |\ ',
+    '| | *',
+    '| |/|',
+    '| * |',
+    '|/| |',
+    '* | |',
+    ' \| |',
+    '  * |',
+    '   \|',
+    '    *',
+  ]],
+  ['input27', [
+    '  *    ',
+    ' /|\   ',
+    '* | *  ',
+    ' \| |\ ',
+    '  * | *',
+    '   \|/ ',
+    '    *  ',
+  ]],
+  ['input28', [
+    '*  ',
+    '|\ ',
+    '| *',
+    '|/|',
+    '* |',
+    '| |',
+    '| *',
+    '|/|',
+    '* |',
+    '| |',
+    '| |',
+    '| |',
+    '* |',
+    ' \|',
+    '  *',
+  ]],
+  ['input29', [
+    '  *   *',
+    '  |  /|',
+    '  | * |',
+    '  |/| |',
+    '  | | *',
+    ' /| |/ ',
+    '* | |  ',
+    '| |/|  ',
+    '| * |  ',
+    '|/  |  ',
+    '*   *  ',
+  ]],
+  ['input30', [
+    '    *  ',
+    '   /|  ',
+    '  / |  ',
+    ' /  |  ',
+    '*   *  ',
+    '|\ /|  ',
+    '| * | *',
+    '| | |/ ',
+    '| | /  ',
+    '| |/|  ',
+    '| | |  ',
+    '|/| |  ',
+    '* | *  ',
+    '  |/   ',
+    '  *    ',
+  ]],
+  ['input31', [
+    '*       *',
+    ' \     /|',
+    '  \   / |',
+    '   \ /  |',
+    '*   \   *',
+    ' \ / \ / ',
+    '  /   /  ',
+    ' / \ / \ ',
+    '*   *   *',
+    '   / \  |',
+    '  /   \ |',
+    ' /     \|',
+    '*       *',
+  ]],
+  ['input32', [
+    '  *  ',
+    ' /|  ',
+    '* |  ',
+    ' \|  ',
+    '  \  ',
+    '  |\ ',
+    '  | *',
+    '  | |',
+    '  | |',
+    '  | |',
+    '  | *',
+    '  |/ ',
+    '  |  ',
+    ' /|  ',
+    '* |  ',
+    '| |  ',
+    '| *  ',
+    '|/   ',
+    '*    ',
+  ]], 
   # ...
   ['input43', [
     '    *',
@@ -415,7 +578,7 @@ namesAndInputs = [
   ]],
 ]  # namesAndInputs
 
-def makePicture(nodes, node2index, edges, slack):
+def makePicture(nodes, node2index, edges, edge_precedences_back_to_front, slack):
   print("        in makePicture")
   # If input was this:
   #       *
@@ -466,6 +629,10 @@ def makePicture(nodes, node2index, edges, slack):
   #                            \|/
   #                             *  
 
+  # Syndrome of a node is the bit pattern of which incident edges exist, in this order:
+  #    501
+  #     @
+  #    432
   syndromes = [[False]*6 for node in nodes]
   for inode0,inode1 in edges:
     irow0,icol0 = nodes[inode0]
@@ -483,7 +650,44 @@ def makePicture(nodes, node2index, edges, slack):
   def syndrome2string(syndrome): return ''.join('1' if x else '0' for x in syndrome)
   print("          syndromes = %r" % ([syndrome2string(syndrome) for syndrome in syndromes],))
 
-    
+  # For now, handle edge overlaps by topsort.
+  # Note that, in general, a topsort may not exist!  But I'm not sure
+  # whether there are any of examples of that in the game.
+
+  def topsort(nodes, edges):
+    # cbb: cycle failure is by stack overflow
+    assert type(nodes) == list
+    preds = dict((node,[]) for node in nodes)
+    for node0,node1 in edges:
+      preds[node1].append(node0)
+    answerList = []
+    answerSet = set()
+    def emitPredsAndSelf(node):
+      if node not in answerSet:
+        for pred in preds[node]:
+          emitPredsAndSelf(pred)
+        assert node not in answerSet
+        answerList.append(node)
+        answerSet.add(node)
+    for node in nodes:
+    #for node in reversed(nodes):
+      emitPredsAndSelf(node)
+    return answerList
+
+  if True:
+    assert topsort([], []) == []
+    assert topsort([0], []) == [0]
+    assert topsort([0,1], [(0,1)]) == [0,1]
+    assert topsort([0,1], [(1,0)]) == [1,0]
+    assert topsort([0,1,2], [(2,1),(0,2)]) == [0,2,1]
+
+  edge_order = topsort(list(set(a for a,b in edge_precedences_back_to_front).union(set(b for a,b in edge_precedences_back_to_front))),
+                       edge_precedences_back_to_front)
+  print("          edge_order = %r" % (edge_order,))
+
+  edge_order = topsort(list(range(len(edges))), edge_precedences_back_to_front)
+  print("          edge_order = %r" % (edge_order,))
+
 
   minrow = min(irow for (irow,icol) in nodes)
   maxrow = max(irow for (irow,icol) in nodes)
@@ -515,8 +719,8 @@ def makePicture(nodes, node2index, edges, slack):
 
 
 
-  #for inode0,inode1 in sorted(edges):
-  for inode0,inode1 in reversed(sorted(edges)):
+  for iedge in edge_order:
+    inode0,inode1  = edges[iedge]
     irow0_in,icol0_in = nodes[inode0]
     irow1_in,icol1_in = nodes[inode1]
     irow0_out = 4 + (irow0_in-minrow)//2 * (6+slack)
@@ -563,12 +767,10 @@ def makePicture(nodes, node2index, edges, slack):
     r'./ \.',
     r'*   *',
     r'|\ /|',
-    r'* * *',
+    r'* @ *',
     r'.\|/.',
     r'..*..',
   ]
-  node_sprite_center_row = 4
-  node_sprite_center_col = 2
   node_sprite_N = [
     r'| | |',
     r'| | |',
@@ -576,33 +778,38 @@ def makePicture(nodes, node2index, edges, slack):
     r'| | |',
     r'* | *',
     r'.\|/.',
-    r'..*..',
+    r'..@..',
   ]
-  node_sprite_N_center_row = 6
-  node_sprite_N_center_col = 2
   node_sprite_SE = [
     r'..*..',
     r'./ \.',
-    r'*   .',
+    r'@   .',
     r'|\  .',
     r'* \ .',
     r'.\ \.',
     r'..\ .',
   ]
-  node_sprite_SE_center_row = 2
-  node_sprite_SE_center_col = 0
   node_sprite_SW = [
     r'..*..',
     r'./ \.',
-    r'.   *',
+    r'.   @',
     r'.  /|',
     r'. / *',
     r'./ /.',
     r'. /..',
   ]
-  node_sprite_SW_center_row = 2
-  node_sprite_SW_center_col = 4
-
+  def findTheAtSign(sprite):
+    answer = None
+    for irow in range(len(sprite)):
+      for icol in range(len(sprite[irow])):
+        if sprite[irow][icol] == '@':
+          assert answer is None
+          answer = (irow,icol)
+    return answer
+  node_sprite_center_row,node_sprite_center_col = findTheAtSign(node_sprite)
+  node_sprite_N_center_row,node_sprite_N_center_col = findTheAtSign(node_sprite_N)
+  node_sprite_SW_center_row,node_sprite_SW_center_col = findTheAtSign(node_sprite_SW)
+  node_sprite_SE_center_row,node_sprite_SE_center_col = findTheAtSign(node_sprite_SE)
 
   nspriterows = len(node_sprite)
   nspritecols = len(node_sprite[0])
@@ -964,39 +1171,72 @@ def process(name, input, slack):
   node2index = dict(((nodes[i],i) for i in range(len(nodes))))
   print("      node2index = %r" % (node2index,))
 
+  def findEdgeEndpoints(input,node2index,irow,icol):
+    if input[irow][icol] == '|':
+      delta = (1,0)
+    elif input[irow][icol] == '/':
+      delta = (1,-1)
+    elif input[irow][icol] == '\\':
+      delta = (1,1)
+    else:
+      return None,None
+    jrow0,jcol0 = irow,icol
+    while input[jrow0][jcol0] != '*':
+      jrow0 -= delta[0]
+      jcol0 -= delta[1]
+    jrow1,jcol1 = irow,icol
+    while input[jrow1][jcol1] != '*':
+      jrow1 += delta[0]
+      jcol1 += delta[1]
+    return node2index[(jrow0,jcol0)], node2index[(jrow1,jcol1)]
+
   # Find the edges (pairs of indices into nodes).
   # If this gets an out-of-bounds exception, it means input was bogus.
   edges = set()
   for irow in range(len(input)):
     for icol in range(len(input[irow])):
+      inode0,inode1 = findEdgeEndpoints(input,node2index,irow,icol)
+      if inode0 is not None:
+        edges.add((inode0,inode1))
+  edges = sorted(edges)  # convert set to list
+  print("      edges = %r" % (edges,))
+  edge2index = dict(((edges[i],i) for i in range(len(edges))))
+  print("      edge2index = %r" % (edge2index,))
+
+  edge_precedences_back_to_front = set()
+  for irow in range(len(input)):
+    for icol in range(len(input[irow])):
       if input[irow][icol] == '|':
-        delta = (1,0)
+        deltarow,deltacol = (1,0)
       elif input[irow][icol] == '/':
-        delta = (1,-1)
+        deltarow,deltacol = (1,-1)
       elif input[irow][icol] == '\\':
-        delta = (1,1)
+        deltarow,deltacol = (1,1)
       else:
         continue
-      jrow0,jcol0 = irow,icol
-      while input[jrow0][jcol0] != '*':
-        jrow0 -= delta[0]
-        jcol0 -= delta[1]
-      jrow1,jcol1 = irow,icol
-      while input[jrow1][jcol1] != '*':
-        jrow1 += delta[0]
-        jcol1 += delta[1]
-      edges.add((node2index[(jrow0,jcol0)], node2index[(jrow1,jcol1)]))
-  print("      edges = %r" % (edges,))
+      for sign in (-1,1):
+        if (irow+sign*deltarow >= 0 and irow+sign*deltarow < len(input) and
+            icol+sign*deltacol >= 0 and icol+sign*deltacol < len(input[irow+sign*deltarow]) and
+            input[irow+sign*deltarow][icol+sign*deltacol] != input[irow][icol] and input[irow+sign*deltarow][icol+sign*deltacol] in '|/\\'):
+            # This edge is under the other edge.
+            edge0 = edge2index[findEdgeEndpoints(input,node2index,irow,icol)]
+            edge1 = edge2index[findEdgeEndpoints(input,node2index,irow+sign*deltarow,icol+sign*deltacol)]
+            edge_precedences_back_to_front.add((edge0,edge1))
+  edge_precedences_back_to_front = sorted(edge_precedences_back_to_front)  # convert set to list
+  print("      edge_precedences_back_to_front = %r" % (edge_precedences_back_to_front,))
 
-  picture = makePicture(nodes, node2index, edges, slack)
+
+  picture = makePicture(nodes, node2index, edges, edge_precedences_back_to_front, slack)
 
   print("    out process(name="+name+")")
 
-slack = 4  # XXX input
+slack = 3  # XXX input
 ninputs = len(namesAndInputs)
 if len(sys.argv) > 1:
   ninputs = int(sys.argv[1])
-for name,input in namesAndInputs[:ninputs]:  # XXX make the range an input
+for i in range(ninputs):
+  name,input = namesAndInputs[i]
+  print("  i = %d" % (i,))
   process(name, input, slack)
 
 
